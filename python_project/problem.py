d34e6cf8a5cf1c878df6955115b638c971e9a1d5
Erfan-Mashayekh <mashayekh.e@gmail.com>
2024-02-09 19:21:25 +0100

    define problem

---
+++ python_project/problem.py
@@ -0,0 +1,36 @@
+class problem():
+    def __init__(self,
+                 hours, 
+                 pv_production, 
+                 electrical_consumption, 
+                 buy_prices, 
+                 sell_prices,
+                 battery_capacity,
+                 levelized_cost_of_storage,
+                 max_charging_rate, 
+                 storage_efficiency):
+        
+        self.hours = hours
+        self.pv_production = pv_production
+        self.electrical_consumption = electrical_consumption
+        self.buy_prices = buy_prices
+        self.sell_prices = sell_prices
+        self.battery_capacity = battery_capacity
+        self.levelized_cost_of_storage = levelized_cost_of_storage
+        self.max_charging_rate = max_charging_rate
+        self.storage_efficiency = storage_efficiency
+
+    def my_function():
+        pass
+
+
+def main():
+    """
+    The main entrypoint for this script
+    Used in the setup.py file
+    """
+    problem.my_function()
+    print('check')
+
+if __name__ == "__main__":
+    main()
